6a62933a87d5cfe5abf0ad076d5fd451f6afa5fd
Alex Martel <13215031+manofthepeace@users.noreply.github.com>
2024-12-10 12:09:41 -0500

    Update to quarkus 3.17

--- src/main/java/org/acme/RegisterCustomModuleCustomizer.java
+++ src/main/java/org/acme/RegisterCustomModuleCustomizer.java
@@ -6,12 +6,14 @@ import org.jboss.logging.Logger;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.quarkus.jackson.ObjectMapperCustomizer;
+import io.quarkus.runtime.annotations.StaticInitSafe;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
 public class RegisterCustomModuleCustomizer implements ObjectMapperCustomizer {
 
+    @StaticInitSafe
     @ConfigProperty(name = "my.variable")
     String testProp;
 
